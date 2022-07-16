4cf114eb31555baf2e446318fcaa998c21a64427
John Ciubuc <john.ciubuc@ttuhsc.edu>
2022-07-15 21:37:58 -0500

    file updated. examples are ready

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # GPT-to-Dalle-Flow
-For circle
+For circle. Generate a GPT prompt, toss it to Dalle, get an image, make it wallpaper-able, and 'apply'.
+
+This doesn't apply the wallpaper, but you could likely do so by adding this at the end:
+
+
+```py
+import ctypes
+ctypes.windll.user32.SystemParametersInfoW(20, 0, "absolute path" , 0)
+```
+
+# Examples
+
+DALLE Prompt: This digital art depicts a light immortal in a xianxia setting. The light immortal is surrounded by a glowing aura, and is floating in the air.
+
+DALLE Prompt: She had always been fascinated by the stories of the great immortals. They were said to be powerful and beautiful, and they lived in a world that was full of color and light. She wanted to be one of them. One day, she found a doorway to their world. It was guarded by a great dragon, but she was not afraid. She boldly stepped through the door and found herself in a beautiful garden.
+
+DALLE Prompt: A rushing waterfall pours over a cliff into a frothing pool far below. The setting sun casts a pink and orange glow on thescene, making the water look like it is on fire.
+
+DALLE Prompt: The eternally young and beautiful creature known as the " Rainbow Immortal " lives in a world of fantasy and magic. She is said to bring good luck and happiness to all who cross her path. Her bright and colorful appearance is a symbol of hope and joy, and her presence is said to bring light and happiness into the lives of those she meets.
+
+DALLE Prompt: This dark hero is clad in steampunk armor, complete with brass goggles and a breathing mask. He carries a largesword at his side, and his trusty robotic companion by his feet. In the background is a steampunk city, filled with airships and dirigibles.
--- main.py
+++ main.py
@@ -5,22 +5,51 @@ Created on Fri Jul 15 18:54:26 2022
 
 @author: inathero
 """
-
+import random
 import openai
 from PIL import Image
 from docarray import Document
 
-openai_secret_key = 'SECRET KEY HERE'
+# ===================
+# Setup
+# ===================
+
+target_resolution = (1920,1080)
+
+# An optional resampling filter. 
+# This can be one of: 
+    # Image.NEAREST (use nearest neighbour)
+    # Image.BILINEAR (linear interpolation)
+    # Image.BICUBIC (cubic spline interpolation)
+    # Image.LANCZOS (a high-quality downsampling filter).
+# If omitted, or if the image has mode “1” or “P”, it is set PIL.Image.NEAREST.
+target_resample = 1
+
+openai_secret_key = 'SECRET-KEY-HERE: https://beta.openai.com/account/api-keys'
 dalle_server_url = 'grpcs://dalle-flow.dev.jina.ai'
 
-prompt = "generate a dalle wallpaper description"
+random_descriptions = []
+random_descriptions.append(['abstract', 'realistic', 'waterpainting', 'digital art', 'photography'])
+random_descriptions.append(['dark', 'bright', 'light', 'dull', 'colourful'])
+random_descriptions.append(['landscape', 'hero', 'anime', 'waterfall', 'immortal'])
+random_descriptions.append(['mythical', 'fantasy', 'steampunk', 'science fiction', 'xianxia'])
+
+# ===================
+# End Setup
+# ===================
+
+descriptions = []
+for i in range(len(random_descriptions)):
+    descriptions.append(random.choice(random_descriptions[i]))
+
+# Random prompt for GPT
+prompt = f'A {descriptions[0]} of a {descriptions[1]} {descriptions[2]} in a {descriptions[3]} setting'
 
 openai.api_key = openai_secret_key
-
 response = openai.Completion.create(
   model="text-davinci-002",
   prompt=prompt,
-  temperature=0.7,
+  temperature=1.0,
   max_tokens=100,
   top_p=1,
   frequency_penalty=0,
@@ -31,9 +60,13 @@ prompt = response['choices'][0]['text'].strip()
 print(f'DALLE Prompt: {prompt}')
 print('Getting goodies from DALLE. This can take like 5 minutes')
 
+# Get a DALLE image
 da = Document(text=prompt).post(dalle_server_url, parameters={'num_images': 1}).matches
+# Upscale that image to 1024x1024
 fav = da[0].post(f'{dalle_server_url}/upscale')
 tensor_d = fav.load_uri_to_image_tensor()
 image = Image.fromarray(tensor_d.tensor)
-image = image.resize((1920,1080), resample=Image.BOX)
+# Upscale image to target resolution
+image = image.resize(target_resolution, resample=target_resample)
+# Save
 image.save('dalle_wallpaper.png')
